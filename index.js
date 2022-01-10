69df6062b177a40ad9dfd7e7500798b78d05cecc
jhessiny <jhessinymattos@gmail.com>
2022-01-10 19:56:55 -0300

    calc logic

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-const calc = document.querySelector(".calculator");
+class Calculator {
+  screen = document.querySelector(".calculator__screen");
+  buttons = document.querySelectorAll(".btn");
+  operation = "";
+
+  constructor() {
+    this.buttons.forEach((btn) =>
+      btn.addEventListener("click", () => {
+        if (btn.dataset.val === "=") this.calculate();
+        if (btn.dataset.val === "AC") this.updateScreen(null, true);
+        if (btn.dataset.val === "DEL") this.del();
+        if (btn.dataset.type === "number" || btn.dataset.type === "op")
+          this.updateScreen(btn.dataset.val);
+      })
+    );
+  }
+
+  calculate() {
+    try {
+      const op = this.screen.textContent.replace("x", "*");
+      const result = eval(op);
+      this.updateScreen(result, true);
+    } catch (err) {
+      alert("Operação inválida");
+    }
+  }
+
+  updateScreen(val, clean = false) {
+    if (clean) this.screen.innerHTML = "";
+    if (val) this.screen.innerHTML += val;
+  }
+
+  del() {
+    const newValue = this.screen.innerHTML.slice(0, -1);
+    this.updateScreen(newValue, true);
+  }
+}
+
+const calc = new Calculator();
